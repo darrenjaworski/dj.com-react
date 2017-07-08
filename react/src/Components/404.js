425a50ec171619aecc524fffdad17ae3708b1fd0
Darren Jaworski <darren.jaworski@vieuxinc.com>
2017-07-08 15:51:51 -0500

    Refined 404 image. New quote.

--- react/src/Components/404.js
+++ react/src/Components/404.js
@@ -13,19 +13,17 @@ class Home extends Component {
       billurl: undefined
     }
   }
-  componentWillMount() {
-    const width = getRandomIntInclusive(400, 700);
-    const height = getRandomIntInclusive(300, 500);
+  componentDidMount() {
+    const width = getRandomIntInclusive(500, 720);
+    const height = getRandomIntInclusive(300, 600);
     const url = `https://www.fillmurray.com/g/${width}/${height}`;
-    var img = new Image();
-    img.src = url;
 
     this.setState({billurl: url});
   }
   render() {
     return (
       <main>
-        <h1>Something strange.</h1>
+        <h1>Human sacrifice, dogs and cats living together... mass hysteria!</h1>
         <img src={this.state.billurl} alt="Bill Murray." />
       </main>
     )
