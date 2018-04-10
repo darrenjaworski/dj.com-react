7da0ecc70a3933dd17214b0d208b3eec0bc917cb
Darren Jaworski <darrenjaworski@gmail.com>
2018-04-09 21:35:17 -0500

    Prettier. Updated css.

--- react/src/App.js
+++ react/src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   BrowserRouter as Router,
   Route,
   NavLink,
   Switch
-} from 'react-router-dom'
+} from "react-router-dom";
 
-import Home from './Components/Home';
-import Projects from './Components/Projects';
-import Journalism from './Components/Journalism';
-import FourOhFour from './Components/404.js';
+import Home from "./Components/Home";
+import Projects from "./Components/Projects";
+import Journalism from "./Components/Journalism";
+import FourOhFour from "./Components/404.js";
 
-import 'normalize.css';
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
@@ -21,16 +20,22 @@ class App extends Component {
         <div>
           <header>
             <nav id="navigation" className="nav">
-              <NavLink to="/" exact activeClassName="active">home</NavLink>
-              <NavLink to="/projects" activeClassName="active">projects</NavLink>
-              <NavLink to="/journalism" activeClassName="active">journalism</NavLink>
+              <NavLink to="/" exact activeClassName="active">
+                home
+              </NavLink>
+              <NavLink to="/projects" activeClassName="active">
+                projects
+              </NavLink>
+              <NavLink to="/journalism" activeClassName="active">
+                journalism
+              </NavLink>
             </nav>
           </header>
           <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route path="/projects" component={Projects}></Route>
-            <Route path="/journalism" component={Journalism}></Route>
-            <Route component={FourOhFour}/>
+            <Route exact path="/" component={Home} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/journalism" component={Journalism} />
+            <Route component={FourOhFour} />
           </Switch>
         </div>
       </Router>
--- react/src/Components/404.js
+++ react/src/Components/404.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 function getRandomIntInclusive(min, max) {
   min = Math.ceil(min);
@@ -11,22 +11,24 @@ class Home extends Component {
     super();
     this.state = {
       billurl: undefined
-    }
+    };
   }
   componentDidMount() {
     const width = getRandomIntInclusive(500, 720);
     const height = getRandomIntInclusive(300, 600);
     const url = `https://www.fillmurray.com/g/${width}/${height}`;
 
-    this.setState({billurl: url});
+    this.setState({ billurl: url });
   }
   render() {
     return (
       <main>
-        <h1>Human sacrifice, dogs and cats living together... mass hysteria!</h1>
+        <h1>
+          Human sacrifice, dogs and cats living together... mass hysteria!
+        </h1>
         <img src={this.state.billurl} alt="Bill Murray." />
       </main>
-    )
+    );
   }
 }
 
