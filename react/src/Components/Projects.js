57aafd0e6d371925c56cde84c679be08ec4eafd5
Darren Jaworski <darrenjaworski@gmail.com>
2018-07-02 22:02:47 -0500

    Remove hard coded journalism and projects section to json data...

--- react/src/Components/Projects.js
+++ react/src/Components/Projects.js
@@ -1,96 +1,23 @@
 import React, { Component } from "react";
 
+import projectData from "../data/projects.json";
+
 class Projects extends Component {
   render() {
+    let projects = projectData.map(d => {
+      return (
+        <div>
+          <h2>
+            <a href={d.link}>{d.title}</a>
+          </h2>
+          <p dangerouslySetInnerHTML={{ __html: d.paragraph }} />
+        </div>
+      );
+    });
     return (
       <main>
         <h1>projects</h1>
-        <h2>
-          <a href="https://github.com/darrenjaworski/d3-reusable">D3Re</a>
-        </h2>
-        <p>
-          A small D3 reusable charts library. Basically I'm working to add some
-          missing .chart() functions in the D3 library.
-        </p>
-        <h2>
-          <a href="https://github.com/darrenjaworski/js-algorithms/">
-            JS algorithms
-          </a>
-        </h2>
-        <p>
-          Working through{" "}
-          <a href="http://algs4.cs.princeton.edu/home/">
-            Algorithms, 4th edition
-          </a> and{" "}
-          <a href="https://www.amazon.com/Introduction-Algorithms-3rd-MIT-Press/dp/0262033844">
-            Introduction to Algorithms, 3rd edition
-          </a> using Javascript and teaching myself all the CS I don't work on
-          in my day job. I'm thinking of expanding this and recreating the data
-          structures and algorithms in Java and Python as well.
-        </p>
-        <h2>
-          <a href="https://docs.google.com/presentation/d/1bOAEXV-vtFnRP4jIZYFUXn1QA5f3u0QDjdme_s-NmEQ/edit?usp=sharing">
-            Code for OKC D3 Talk
-          </a>
-        </h2>
-        <p>Quick talk I gave to Code for OKC about D3.js</p>
-        <h2>
-          <a href="https://github.com/darrenjaworski/coffee-lovers-okc">
-            OKC coffee
-          </a>
-        </h2>
-        <p>
-          An app to explore coffee shops in Oklahoma City. Designed and built
-          from the ground up to be a hub for coffee people of OKC. However,
-          a lot more work needs to be done on this project. I need to finish
-          collecting data for coffee shops, to develop a suggestion system to
-          lead you to the perfect cup in town, and to actually launch the thing.
-          I'll take a couple of weekends and finish this project over a cup of
-          coffee.
-        </p>
-        <h2>
-          <a href="http://darrenjaworski.github.io/simplestats/">
-            stats made simple
-          </a>
-        </h2>
-        <p>
-          A series of explainers and tutorials for aspiring statisticians. This
-          is another project that has significantly more work to be done. I'll
-          need two cups of coffee.
-        </p>
-        <h2>
-          <a href="http://bl.ocks.org/darrenjaworski/">
-            earthquakes in Oklahoma
-          </a>
-        </h2>
-        <p>
-          I felt an earthquake during lunch. I decided to put a couple of
-          minutes into a{" "}
-          <a href="http://bl.ocks.org/darrenjaworski/eeeb26283484184984e3">
-            series
-          </a>{" "}
-          of{" "}
-          <a href="http://bl.ocks.org/darrenjaworski/caf09001e2812c4e388e">
-            maps
-          </a>{" "}
-          and{" "}
-          <a href="http://bl.ocks.org/darrenjaworski/23cbba3bfbe564113fbc">
-            charts
-          </a> that touch on the swarm of induced earthquakes in Oklahoma.{" "}
-          <a href="http://bl.ocks.org/darrenjaworski/">(Even more.)</a> Each
-          visualization is updated with real-time earthquake data. I will
-          include more data layers as they become available.
-        </p>
-        <h2>
-          <a href="http://bl.ocks.org/darrenjaworski/f2540e53c4e327a145d0">
-            legislature voting trends
-          </a>
-        </h2>
-        <p>
-          Similarity matrix of the 2015 Oklahoma Legislature voting trends. This
-          small project basically answers the question: which legislators vote
-          like each other?
-        </p>
+        {projects}
       </main>
     );
   }
