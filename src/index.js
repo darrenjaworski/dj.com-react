65831248f67ef27dacb792ace0490247e82e31ec
Darren Jaworski <darrenjaworski@gmail.com>
2019-11-15 18:51:15 -0600

    Unnecessary files and references.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,4 @@ import App from "./App";
 import "normalize.css";
 import "./index.css";
 
-import { unregister } from "./registerServiceWorker";
-
 ReactDOM.render(<App />, document.getElementById("root"));
-unregister();
