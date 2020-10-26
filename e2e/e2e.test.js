a54c8e87874e73a763a34e69b1f3cb54cbcde7c6
Darren Jaworski <darrenjaworski@gmail.com>
2020-10-25 21:31:35 -0500

    Visual regression testing...

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,12 +1,15 @@
-const path = require("path");
+describe("visual regression testing", () => {
+  const routes = ["projects", "", "journalism"];
 
-describe("app", () => {
-  beforeEach(async () => {
-    await page.goto("http://localhost:3000");
-  });
+  routes.forEach((route) => {
+    it(`${route ? route : "app"} matches page render`, async () => {
+      await page.goto(`http://localhost:3000/${route}`);
+      const image = await page.screenshot({
+        path: `e2e/__snapshots__/${route ? route : "app"}.png`,
+        fullPage: true,
+      });
 
-  it("to match page renders", async () => {
-    await page.screenshot({ path: "e2e/__snapshots__/base.png" });
-    expect(true).toBe(true);
+      expect(image).toMatchImageSnapshot();
+    });
   });
 });
