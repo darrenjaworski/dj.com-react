321f4fb9b86ddb67d96970c639fdaa8bf0cd4a6e
Darren Jaworski <darrenjaworski@gmail.com>
2025-07-12 22:52:27 -0500

    chore: setup gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,21 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+const config = defineConfig({
   plugins: [react()],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
-})
+});
+
+// @ts-expect-error process not defined
+if (process?.env?.PUBLIC_URL) {
+  // @ts-expect-error process not defined
+  config.base = process.env.PUBLIC_URL;
+}
+
+export default config;
