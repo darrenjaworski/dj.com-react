cd6ca37e0ff0849c86da987f744402239fe600e2
Darren Jaworski <darrenjaworski@gmail.com>
2025-07-15 22:20:56 -0500

    chore: remove build step, build into preview command

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,7 +64,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: "npm run preview",
+    command: "npm run build && npm run preview",
     url: "http://localhost:4173",
     reuseExistingServer: !process.env.CI,
   },
