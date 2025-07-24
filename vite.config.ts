e87e8ac280a5c50f4ae674dc046eb0f553610eb3
Darren Jaworski <darrenjaworski@gmail.com>
2025-07-23 21:43:45 -0500

    chore: update coverage

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,12 @@ const config = defineConfig({
       "**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*",
       "**/tests/**/*.spec.ts", // Exclude Playwright e2e tests
     ],
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "html", "lcov"],
+      reportsDirectory: "./coverage",
+      include: ["src/**/*"],
+    },
   },
 });
 
